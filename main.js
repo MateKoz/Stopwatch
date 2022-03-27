e994360b7b24431c5c302bd35b57bad754f3f979
MateuszKozlowski <mateuszkozlowski@climatik.pl>
2022-03-28 01:19:48 +0200

    create script in JS

---
+++ main.js
@@ -0,0 +1,113 @@
+//POBIERAMY POTRZEBNE ZMIENNE
+const startBtn = document.querySelector(`.start`)
+const pauseBtn = document.querySelector(`.pause`)
+const stopBtn = document.querySelector(`.stop`)
+const resetBtn = document.querySelector(`.reset`)
+const historyBtn = document.querySelector(`.history`)
+const stopwatch = document.querySelector(`.stopwatch`)
+const time = document.querySelector(`.time`)
+const timeList = document.querySelector(`.time-list`)
+const infoBtn = document.querySelector(`.info`)
+const showInfo = document.querySelector(`.modal-shadow`);
+const closeInfo = document.querySelector(`.close`)
+
+//FUNKCJA, KTORA WLACZA  I WYŁACZA MODAL Z INFORMACJAMI
+const showModal = () => {
+    if (!(showInfo.style.display === 'block')) {
+        showInfo.style.display = 'block'
+    } else {
+        showInfo.style.display = 'none'
+    }
+    showInfo.classList.toggle('modal-animation')
+}
+
+let countTime;
+let minutes = 0;
+let seconds = 0;
+
+let timesArr = [];
+
+//FUNKCJA URUCHAMIA SETINTERVAL, KTORY ZMIENIA NASZE MINUTES I SECONDS O JEDEN
+// CO JEDNA SEKUNDE
+
+const handleStart = () => {
+    clearInterval(countTime)
+    countTime = setInterval(() => {
+
+        if (seconds < 9) {
+            seconds++
+            time.textContent = `${minutes}:0${seconds}`
+        } else if (seconds >= 9 && seconds < 59) {
+            seconds++
+            time.textContent = `${minutes}:${seconds}`
+        } else {
+            minutes++
+            seconds = 0
+            time.textContent = `${minutes}:00`
+        }
+    }, 100)
+}
+
+//FUNKCJA PAUZUJACA ODLICZANIE, POPRZEZ ZATRZYMANIE SETINTERVAL
+const handlePause = () => {
+    clearInterval(countTime)
+}
+
+//FUNKCJA ZATRZYMUJACA DZIALANIE HANDLESTART, PRZYWRACAJACA WARTOSCI DOMYSLNE NA 0:00
+const handleStop = () => {
+
+    stopwatch.textContent = `Ostatni czas: ${time.textContent}`;
+
+    if (time.textContent !== `0:00`) {
+        stopwatch.style.visibility = 'visible';
+        pushArr();
+    }
+
+    clearAll()
+}
+
+//FUNKCJA ZAPICUJACA W TABLICY ZATRZYMANE WYNIKI.
+const pushArr = () => {
+    timesArr.push(time.textContent)
+}
+
+//FUNKCJA RESETUJACA WSZYSTKIE WARTOSCI DO STANU POCZATKOWEGO
+const handleReset = () => {
+    stopwatch.style.visibility = 'hidden';
+    timesArr = [];
+    clearAll()
+
+}
+//FUNKCJA TWORZACA WSPOLNE WLASCIWOSCI, BY NIE POWTARZAC KODU
+const clearAll = () => {
+    clearInterval(countTime)
+    seconds = 0;
+    minutes = 0
+    time.textContent = `0:00`
+    timeList.textContent = '';
+}
+
+
+//FUNKCJA POKAZUJE HISTORIE WSZYSTKICH ZASTOPOWANYCH POMIAROW.
+const showHistory = () => {
+    timeList.textContent = '';
+    let num = 1
+
+    timesArr.forEach(time => {
+        const newTime = document.createElement('li');
+        newTime.innerHTML = `Pomiar nr ${num} wynosił: <span>${time}</span>`
+        timeList.appendChild(newTime)
+        num++
+    })
+}
+
+//NASLUCHIWANIA
+historyBtn.addEventListener('click', showHistory)
+closeInfo.addEventListener('click', showModal)
+resetBtn.addEventListener('click', handleReset)
+infoBtn.addEventListener('click', showModal)
+startBtn.addEventListener('click', handleStart)
+pauseBtn.addEventListener('click', handlePause)
+stopBtn.addEventListener('click', handleStop)
+window.addEventListener('click',
+    e => e.target === showInfo ? showModal() : false)
